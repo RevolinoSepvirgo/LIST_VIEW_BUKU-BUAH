07dad5290aa1cb6156ca7dda1810d64930cbefb2
RevolinoSepvirgo <revolinosepvirgo02@gmail.com>
2024-09-12 17:29:08 +0700

    update

---
+++ app/src/main/java/com/revo/listviewactivity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.revo.listviewactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnhewan: Button
+    private lateinit var btnbuku : Button
+    private lateinit var btnbuah : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        btnhewan = findViewById(R.id.btnhewan)
+        btnbuku = findViewById(R.id.btnbuku)
+        btnbuah = findViewById(R.id.btnbuah)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnhewan.setOnClickListener() {
+            val intent = Intent(this@MainActivity, hewan::class.java)
+            startActivity(intent)
+        }
+        btnbuku.setOnClickListener() {
+            val intent = Intent(this@MainActivity, RecycleviewAcitivity::class.java)
+            startActivity(intent)
+        }
+        btnbuah.setOnClickListener() {
+            val intent = Intent(this@MainActivity, RecycleBuahActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
